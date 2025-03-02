40ad6198498c9ee681322b437c7e33bae9c84713
madiha-m <madiha.muhammad.dev@gmail.com>
2025-03-02 22:59:18 +0500

    feat(Home): register Blog model with admin panel and customize display

--- Backend/Home/admin.py
+++ Backend/Home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Blog
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at','update_at') #Fields to display in the list view
+    search_fields = ('title','author', 'content') # Fields to search in the admin interface
+    list_filter = ('created_at', 'author') #Fields to filter in the list view in admin interface
+    ordering = ('created_at') # Default ordering of the list view
+    
+    
