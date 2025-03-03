f98d75d51dbc87fc0309c874adb61213b1a0c85a
madiha-m <madiha.muhammad.dev@gmail.com>
2025-03-03 06:07:52 +0500

    refactor(Home): update BlogAdmin configuration in admin panel

--- Backend/Home/admin.py
+++ Backend/Home/admin.py
@@ -6,9 +6,9 @@ from .models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at','update_at') #Fields to display in the list view
+    list_display = ('title', 'author', 'created_at') #Fields to display in the list view
     search_fields = ('title','author', 'content') # Fields to search in the admin interface
     list_filter = ('created_at', 'author') #Fields to filter in the list view in admin interface
-    ordering = ('created_at') # Default ordering of the list view
+    ordering = ('-created_at',) # Default ordering of the list view
     
     
